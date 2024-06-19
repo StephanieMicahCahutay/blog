c5c10d438216b596963cc7c6babf2abe8bda5fb9
Stephanie Micah Cahutay <stephaniemicah.cahutay@cit.edu>
2024-06-20 00:49:31 +0800

    Update post class

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,19 +1,21 @@
+# app/models/post.rb
 class Post < ApplicationRecord
-    validates :title, presence: true, length: { minimum: 5, maximum: 100 }
-    validates :body, presence: true, length: { minimum: 10 }
-    validates :author, presence: true
-    validates :published_at, presence: true
-    validates :status, presence: true, inclusion: { in: %w[draft published] }
+  has_many :comments, dependent: :destroy
   
-    # Custom validation for title uniqueness on create
-    validate :title_must_be_unique, on: :create
-  
-    private
-  
-    def title_must_be_unique
-      if Post.exists?(title: title)
-        errors.add(:title, "must be unique. This title has already been taken.")
-      end
+  validates :title, presence: true, uniqueness: true
+  validates :body, presence: true, length: { minimum: 10 }
+  validates :author, presence: true, length: { minimum: 2 }
+  validates :published_at, presence: true
+  validates :status, presence: true, inclusion: { in: %w[draft published] }
+
+  # Custom validation for title uniqueness on create
+  validate :title_must_be_unique, on: :create
+
+  private
+
+  def title_must_be_unique
+    if Post.exists?(title: title)
+      errors.add(:title, "must be unique. This title has already been taken.")
     end
   end
-  
+end
